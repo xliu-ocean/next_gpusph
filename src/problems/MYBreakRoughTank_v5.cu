74e67787e41a24b205154358dd9d12db90884af6
Xiaohui Liu <liux8@oregonstate.edu>
2026-01-05 15:23:10 -0800

    revise the wall in the end, 3f-ii,++

--- src/problems/MYBreakRoughTank_v5.cu
+++ src/problems/MYBreakRoughTank_v5.cu
@@ -269,7 +269,7 @@ MYBreakRoughTank_v5::MYBreakRoughTank_v5(GlobalData *_gdata) : Problem(_gdata)
 
                 // this plane cuts the lateral walls below the sloping ground
                 plane = addPlane(-sin(beta_2), 0, cos(beta_2),
-                        slope_origin.x*sin(beta_2) + 2*(m_deltap + box_thickness*cos(beta_2)),
+                        slope_origin_2.x*sin(beta_2) + 2*(m_deltap + box_thickness*cos(beta_2)),
                         FT_UNFILL);
 
                 setEraseOperation(plane, ET_ERASE_BOUNDARY);
