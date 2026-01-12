da51941bb94bd8c47ca331c30432066733cc7a8f
Xiaohui Liu <liux8@oregonstate.edu>
2026-01-12 14:42:51 -0800

    add endwall 5b-1

--- src/problems/MYBreakRoughTank_v6.cu
+++ src/problems/MYBreakRoughTank_v6.cu
@@ -239,7 +239,7 @@ MYBreakRoughTank_v6::MYBreakRoughTank_v6(GlobalData *_gdata) : Problem(_gdata)
         //                Point(make_double3(0,0,0) + make_double3(0, ly, -box_thickness)),
         //                lx + paddle_origin.x, box_thickness, lz);
 		// end wall
-                wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
+                GeometryID wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
                         //Point(m_origin + make_double3(0, ly, -box_thickness)),
                         Point(make_double3(0,0,0) + make_double3(h_length+slope_length+slope2_length,0, -box_thickness)),
         		box_thickness, ly,  lz);
