a2be29220cf2a92018d7d978820b52f2f7ef1c5a
Xiaohui Liu <liux8@oregonstate.edu>
2026-01-12 16:03:04 -0800

    add water to create DamBreak

--- src/problems/MYBreakRoughTank_v6.cu
+++ src/problems/MYBreakRoughTank_v6.cu
@@ -283,27 +283,29 @@ MYBreakRoughTank_v6::MYBreakRoughTank_v6(GlobalData *_gdata) : Problem(_gdata)
 
                 setEraseOperation(plane, ET_ERASE_FLUID);
         }
-	addDEMFluidBox(0.8);
+	float water_height = 0.8;
+	addDEMFluidBox((water_height);
 	addExtraWorldMargin(5*m_deltap);
 	GeometryID fluid;
 	float z = 0;
 	int n = 0;
 	while (z < H) {
-		z = n*(m_deltap+1e-6) + 1.5*r0 +r0;    //z = n*m_deltap + 1.5*r0;
+		//z = n*(m_deltap+1e-6) + 1.5*r0 +r0;    //z = n*m_deltap + 1.5*r0;
+		z = n*(m_deltap+1e-6) + water_height;
 		//float x = paddle_origin.x + (z - paddle_origin.z)*tan(amplitude) + 1.0*r0/cos(amplitude);
 		float x = paddle_origin.x +r0;
 		//float l = h_length + z/tan(beta) - 1.5*r0/sin(beta) - x;
 		//float l = h_length;
 		float l;
-                if (z <= 0.6f) {
-                     l = h_length + z/tan(beta) - 1.5*r0/sin(beta) - x;
+                //if (z <= 0.6f) {
+                //     l = h_length + z/tan(beta) - 1.5*r0/sin(beta) - x;
                 //} else if (z <= 0.8f) {
                 //     l = h_length + 0.5f/tan(beta) + (z-0.5f)/tan(beta_2) - 1.5*r0/sin(beta_2) - x;
-                } else {
+                //} else {
                      l = h_length-10;
-                }
-	//	fluid = addRect(GT_FLUID, FT_SOLID, Point(x,  r0, z),
-	//			10.0, ly-2.0*r0);
+                //}
+		fluid = addRect(GT_FLUID, FT_SOLID, Point(x,  r0, z),
+				l, ly-2.0*r0);
 		n++;
 	 }
 // activate the solid obstacle
