b3814fca7a23e944e6d5e3293e8c814973e61bee
Xiaohui Liu <liux8@oregonstate.edu>
2026-01-26 07:00:20 -0800

    add a wall at north to fill the holes on the DEM wall, v7-3a-iii, 2

--- src/problems/MYBreakRoughTank_v7.cu
+++ src/problems/MYBreakRoughTank_v7.cu
@@ -252,7 +252,7 @@ MYBreakRoughTank_v7::MYBreakRoughTank_v7(GlobalData *_gdata) : Problem(_gdata)
                 //        Point(make_double3(0,0,0) + make_double3(-box_thickness-m_deltap,0, water_height+m_deltap)),
                 //        box_thickness, ly,  lz-water_height);
 		//top cover
-		GeometryID wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
+		wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
                 //        //Point(m_origin + make_double3(0, ly, -box_thickness)),
                         Point(make_double3(0,0,0) + make_double3(-box_thickness, -m_deltap, 2.85)),
                         lx + 2*box_thickness, ly + 2*m_deltap, box_thickness);
