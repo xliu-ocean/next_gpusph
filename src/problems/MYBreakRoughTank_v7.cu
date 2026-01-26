c15afa3205faf20237981443d737cf6fbe1c8eea
Xiaohui Liu <liux8@oregonstate.edu>
2026-01-26 12:57:16 -0800

    change the cobble to fac=0.1, v7-3b-i

--- src/problems/MYBreakRoughTank_v7.cu
+++ src/problems/MYBreakRoughTank_v7.cu
@@ -69,7 +69,8 @@ MYBreakRoughTank_v7::MYBreakRoughTank_v7(GlobalData *_gdata) : Problem(_gdata)
         beta_2 = 11.30993*M_PI/180.0; //b1/eta for run-up slope
 
 	// add DEM
-	const string dem_file = get_option("dem", "cobble_surface_with_slope_v3.txt");
+	//const string dem_file = get_option("dem", "cobble_surface_with_slope_v3.txt");
+	cconst string dem_file = get_option("dem", "obble_surface_with_slope_v4_0.1fac.txt");
 
 
 	SETUP_FRAMEWORK(
