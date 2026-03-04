33376b17d4c3ce262270a377e36cb4996012b9ed
Xiaohui Liu <liux8@oregonstate.edu>
2026-03-04 11:36:52 -0800

    change time step and t_end

--- src/problems/MYFlatRoughTank_v1.cu
+++ src/problems/MYFlatRoughTank_v1.cu
@@ -145,7 +145,7 @@ MYFlatRoughTank_v1::MYFlatRoughTank_v1(GlobalData *_gdata) : Problem(_gdata)
 	paddle_width = ly - 10*r0;
 	paddle_tstart=0.5f;
 	paddle_origin = make_double3(5*r0, 6*r0, 4*r0+1e-8);
-	paddle_tend = 30.0f;
+	paddle_tend = 3.0f;
 	// The stroke value is given at free surface level H
 	// float stroke = 0.2;
 	// m_mbamplitude is the maximal angular value for paddle angle
@@ -158,7 +158,7 @@ MYFlatRoughTank_v1::MYFlatRoughTank_v1(GlobalData *_gdata) : Problem(_gdata)
 
 	// Drawing and saving times
 
-	add_writer(VTKWRITER, .25);  //second argument is saving time in seconds
+	add_writer(VTKWRITER, .05);  //second argument is saving time in seconds
 
 	// Name of problem used for directory creation
 	//m_name = "MYFlatRoughTank_v1";
