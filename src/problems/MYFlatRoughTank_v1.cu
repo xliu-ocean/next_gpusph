b959a64c229b1aa60ec7427fb2e8ad863eaf3e6d
Xiaohui Liu <liux8@oregonstate.edu>
2026-03-05 15:46:50 -0800

    change the DEM to make the slope flat,without cobble, fac=0.0

--- src/problems/MYFlatRoughTank_v1.cu
+++ src/problems/MYFlatRoughTank_v1.cu
@@ -69,7 +69,7 @@ MYFlatRoughTank_v1::MYFlatRoughTank_v1(GlobalData *_gdata) : Problem(_gdata)
         beta_2 = 11.30993*M_PI/180.0; //b1/eta for run-up slope
 
 	// add DEM
-	const string dem_file = get_option("dem", "preprocessing/flat_bottom/cobble_surface_with_flat_sand_v1_fac1.0.txt");
+	const string dem_file = get_option("dem", "preprocessing/flat_bottom/cobble_surface_with_flat_sand_v1_fac0.0.txt");
 
 
 	SETUP_FRAMEWORK(
