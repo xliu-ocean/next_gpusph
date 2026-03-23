68f754c768d3a063dc5742af16356a1406d1287d
Xiaohui Liu <liux8@oregonstate.edu>
2026-03-23 00:53:13 -0700

    revise the right wall and the top cover, make length on y dir = 2.01 ;2b

--- src/problems/MYFlatRoughTank_v1.cu
+++ src/problems/MYFlatRoughTank_v1.cu
@@ -232,11 +232,12 @@ MYFlatRoughTank_v1::MYFlatRoughTank_v1(GlobalData *_gdata) : Problem(_gdata)
 
         //        const double wall_height = paddle_length + box_thickness + (lz - paddle_length)/3.0;
 	//	cout << "\nwall height: " << wall_height << "\n";
-                // close wall
+                // end (right) wall
                 GeometryID wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
                 //        //Point(m_origin + make_double3(0, ly, -box_thickness)),
                         Point(make_double3(0,0,0) + make_double3(h_length+slope_length+slope2_length-m_deltap+1.001398e-2,0, -box_thickness)),
-                        box_thickness, ly,  lz+box_thickness);
+                        box_thickness, ly+0.01,  lz+box_thickness);
+		// close wall
 		wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
                 //        //Point(m_origin - make_double3(0, box_thickness, box_thickness)),
                         Point(make_double3(0,0,0) - make_double3(box_thickness+m_deltap, -m_deltap, -m_deltap)),
@@ -261,7 +262,7 @@ MYFlatRoughTank_v1::MYFlatRoughTank_v1(GlobalData *_gdata) : Problem(_gdata)
 		wall = addBox(GT_FIXED_BOUNDARY, FT_BORDER,
                         //Point(m_origin + make_double3(0, ly, -box_thickness)),
                         Point(make_double3(0,0,0) + make_double3(-box_thickness, -m_deltap, lz)),
-                        lx + 2*box_thickness, ly + 2*m_deltap, box_thickness);
+                        lx + 2*box_thickness, ly + 2*m_deltap + 0.01, box_thickness);
         }
 	GeometryID fluid;
         float z = 0;
